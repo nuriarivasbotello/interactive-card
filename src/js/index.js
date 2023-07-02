7ded1c6e650b8f45a0840e33046a17169b4f2618
nuriarivasbotello <nuriarivasbotello@gmail.com>
2023-07-02 21:21:02 +0200

    +

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+const formElement = document.getElementById('form');
+const inputNameElement = document.getElementById('name');
+
+inputNameElement.addEventListener('change', event => {
+  console.log(event.target.value);
+});
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(event.target.name.value);
+});
