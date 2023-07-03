eab778cc08d412babcbeac357d1701203c9f7937
nuriarivasbotello <nuriarivasbotello@gmail.com>
2023-07-03 08:48:37 +0200

    +

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 const formElement = document.getElementById('form');
-const inputNameElement = document.getElementById('name');
+const inputNameElement = document.getElementById('card-name-form');
+const formNameElement = document.getElementById('card-name');
+const inputNumberElement = document.getElementById('card-number-form');
+const formNumberElement = document.getElementById('card-number');
 
-inputNameElement.addEventListener('change', event => {
+formElement.addEventListener('input', event => {
+  formNameElement.textContent = inputNameElement.value;
+  formNumberElement.textContent = inputNumberElement.value;
   console.log(event.target.value);
 });
 formElement.addEventListener('submit', event => {
